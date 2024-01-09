3752bc466d27896a0b35f3b95cdb01eea768fcbb
rahim <umudyan2014@gmail.com>
2024-01-09 13:32:29 -0100

    s

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -13,49 +13,50 @@ import { Link } from "react-router-dom";
 
 export const Project = () => {
   return (
-    <section className="project" id="projects">
-      <div className="main-text">
-        <h1>
-          Projects <span> .</span>
-        </h1>
-        <p>
-          Bringing Your Ideas To Life <br /> Through Me And My Experience
-        </p>
-      </div>
-      <div className="container">
-        <div className="main-item">
-          <div className="item">
-            <Link to="https://ai-rahimwws.vercel.app/">
-              <img src={project_1} alt="" />
-              <p>Web Site</p>
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="https://www.figma.com/file/hN0wFO0HoBgWZVaTG5CYiW/Smart-Home-App-(Community)?type=design&mode=design&t=v5kCxoS9m2Gs821r-0">
-              <img src={project_2} alt="" />
-              <p>UI/UX design</p>
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="https://nike-page-phi.vercel.app/">
-              <img src={project_3} alt="" />
-              <p>Web site</p>
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="https://www.figma.com/file/eh6VqOuCkCAJJ8gU1rtNGA/Fashion?type=design&node-id=0-1&mode=design&t=v5kCxoS9m2Gs821r-0">
-              <img src={project_4} alt="" />
-              <p>UI/UX design</p>
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="https://www.figma.com/file/3T18WJqTgJBXRTceNvOAX7/e-commerce?type=design&node-id=2-2&mode=design&t=v5kCxoS9m2Gs821r-0">
-              <img src={project_5} alt="" />
-              <p>UI/UX design</p>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    <></>
+    // <section className="project" id="projects">
+    //   <div className="main-text">
+    //     <h1>
+    //       Projects <span> .</span>
+    //     </h1>
+    //     <p>
+    //       Bringing Your Ideas To Life <br /> Through Me And My Experience
+    //     </p>
+    //   </div>
+    //   <div className="container">
+    //     <div className="main-item">
+    //       <div className="item">
+    //         <Link to="https://ai-rahimwws.vercel.app/">
+    //           <img src={project_1} alt="" />
+    //           <p>Web Site</p>
+    //         </Link>
+    //       </div>
+    //       <div className="item">
+    //         <Link to="https://www.figma.com/file/hN0wFO0HoBgWZVaTG5CYiW/Smart-Home-App-(Community)?type=design&mode=design&t=v5kCxoS9m2Gs821r-0">
+    //           <img src={project_2} alt="" />
+    //           <p>UI/UX design</p>
+    //         </Link>
+    //       </div>
+    //       <div className="item">
+    //         <Link to="https://nike-page-phi.vercel.app/">
+    //           <img src={project_3} alt="" />
+    //           <p>Web site</p>
+    //         </Link>
+    //       </div>
+    //       <div className="item">
+    //         <Link to="https://www.figma.com/file/eh6VqOuCkCAJJ8gU1rtNGA/Fashion?type=design&node-id=0-1&mode=design&t=v5kCxoS9m2Gs821r-0">
+    //           <img src={project_4} alt="" />
+    //           <p>UI/UX design</p>
+    //         </Link>
+    //       </div>
+    //       <div className="item">
+    //         <Link to="https://www.figma.com/file/3T18WJqTgJBXRTceNvOAX7/e-commerce?type=design&node-id=2-2&mode=design&t=v5kCxoS9m2Gs821r-0">
+    //           <img src={project_5} alt="" />
+    //           <p>UI/UX design</p>
+    //         </Link>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
